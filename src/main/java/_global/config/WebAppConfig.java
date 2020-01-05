4151153788a71c135ea21d5bab34c90bb4ec26e2
Dennis Chung <c10381@gmail.com>
2020-01-05 20:48:31 +0800

    First Push, Basic Environment.

---
+++ src/main/java/_global/config/WebAppConfig.java
@@ -0,0 +1,60 @@
+package _global.config;
+
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.iiiedu.EEIT110.ProjectPizzaWorks")
+public class WebAppConfig implements WebMvcConfigurer {
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	//檔案相關(P382)
+	@Bean
+	public CommonsMultipartResolver multipartResolver() {
+		CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+		resolver.setDefaultEncoding("UTF-8");
+		resolver.setMaxUploadSize(81920000);
+		return resolver;
+	}
+	//Bean直接轉成JSON(要在Bean前加＠JsonIgnoreProperties("Name"))
+	//可用Rest來做
+	@Bean
+	public MappingJackson2JsonView jsonView() {
+		MappingJackson2JsonView view = new MappingJackson2JsonView();
+		view.setPrettyPrint(true);
+		return view;
+	}
+	@Bean
+	public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setContentNegotiationManager(manager);
+		ArrayList<View> views = new ArrayList<>();
+		views.add(jsonView());
+		resolver.setDefaultViews(views);
+		return resolver;
+	}
+}
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,8 @@
+﻿<%@ page contentType="text/html; charset=UTF-8" session="false"%>
+<html> 
+<head>
+<title>Index</title>   
+</head>
+<body>
+</body>
+</html>
