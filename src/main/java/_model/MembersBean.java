3e2332a3778b69099d13cd058efaf5fac2acb530
Dennis Chung <c10381@gmail.com>
2020-01-07 13:42:28 +0800

    change memberBean to Hibernate ver.

--- src/main/java/_model/MembersBean.java
+++ src/main/java/_model/MembersBean.java
@@ -3,8 +3,17 @@ package _model;
 import java.io.Serializable;
 import java.sql.Timestamp;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table
 public class MembersBean implements Serializable{
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer memberId;
 	private String firstName;
 	private String lastName;
