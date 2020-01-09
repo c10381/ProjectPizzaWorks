d7396a9760462c6b1066e0b5a4f2c7c26d22d037
Leon <iiiedujava@gmail.com>
2020-01-09 19:42:37 +0800

    Auto stash before merge of "feature-memberSystem" and "origin/feature-memberSystem"

--- src/main/java/memberSystem/controller/CustomerController.java
+++ src/main/java/memberSystem/controller/CustomerController.java
@@ -2,6 +2,7 @@ package memberSystem.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,11 +20,9 @@ public class CustomerController {
 	}
 	
 	  
-	@RequestMapping(method=RequestMethod.POST)
-	public String addCustomer(MembersBean mem) {
-		
+	@RequestMapping(value="/member/add", method=RequestMethod.POST)
+	public String addCustomer(@ModelAttribute("MembersBean") MembersBean mem) {	
 		service.addCustomer(mem);
-		
 		return "addCustomer";
 	}
 	
