9de987dbdb7a32e8fad43f9b1ece20a2e44dfd6b
Dennis Chung <c10381@gmail.com>
2020-01-13 11:12:18 +0800

    Add Restful prop

--- src/main/java/_global/config/WebAppInitializer.java
+++ src/main/java/_global/config/WebAppInitializer.java
@@ -3,6 +3,7 @@ package _global.config;
 import javax.servlet.Filter;
 
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -31,6 +32,7 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
 		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
 		characterEncodingFilter.setEncoding("UTF-8");
 		characterEncodingFilter.setForceEncoding(true);
-		return new Filter[] {characterEncodingFilter};
+		HiddenHttpMethodFilter hiddenHttpMethodFilter=new HiddenHttpMethodFilter();
+		return new Filter[] {characterEncodingFilter,hiddenHttpMethodFilter};
 	}
 }
