5c7ee741d30a78b66557d051af2880607cf73ab0
Dennis Chung <c10381@gmail.com>
2020-01-16 20:31:32 +0800

    small fix

--- src/main/java/memberSystem/controller/CustomerController.java
+++ src/main/java/memberSystem/controller/CustomerController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import _model.MembersBean;
 import memberSystem.service.CustomerService;
